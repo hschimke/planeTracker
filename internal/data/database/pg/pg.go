ac8a7067a855baf21597e09e28a5cac0c0e48d9e
Henry Schimke <henry@azule.info>
2022-04-06 13:05:00 -0500

    start building out postgres

--- internal/data/database/pg/pg.go
+++ internal/data/database/pg/pg.go
@@ -1,3 +1,65 @@
 package pg
 
-type PostgresDatabase struct{}
+import (
+	"context"
+	"github.com/hschimke/planeTracker/internal/data/model"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+const (
+	getUserFlightsSql string = "SELECT * FROM flights WHERE user_id = $1"
+	addFlightSql      string = "INSERT INTO flights("
+)
+
+type PostgresDatabase struct {
+	db *pgxpool.Pool
+}
+
+func NewPostgresDatabase(connectionString string) *PostgresDatabase {
+	pool, poolErr := pgxpool.Connect(context.Background(), connectionString)
+	if poolErr != nil {
+		panic(poolErr.Error())
+	}
+	return &PostgresDatabase{
+		db: pool,
+	}
+}
+
+func setupDatabase(pool *pgxpool.Pool) {
+	//TODO setup database (tables index etc)
+}
+
+func (p *PostgresDatabase) GetFlightsForUser(user model.User) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p *PostgresDatabase) AddFlight(flight model.Flight) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p *PostgresDatabase) DeleteFlight(flight model.Flight) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p *PostgresDatabase) UpdateFlight(flight model.Flight) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p *PostgresDatabase) GetUser(user model.User) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p *PostgresDatabase) DeleteUser(user model.User) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p *PostgresDatabase) UpdateUser(user model.User) {
+	//TODO implement me
+	panic("implement me")
+}
