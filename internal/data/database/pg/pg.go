eb0169a0538980190af1ddc3f15163ff765f6764
Henry Schimke <henry@azule.info>
2022-04-06 15:16:20 -0500

    Add db setup tasks and SQL

--- internal/data/database/pg/pg.go
+++ internal/data/database/pg/pg.go
@@ -3,6 +3,7 @@ package pg
 import (
 	"context"
 	"github.com/hschimke/planeTracker/internal/data/model"
+	"github.com/jackc/pgx/v4"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 )
@@ -18,53 +19,53 @@ const (
 )
 
 const (
-	createUserTableSql      string = ""
-	createUserTableIndexSql string = ""
+	createUserTableSql      string = "CREATE TABLE IF NOT EXISTS users (id UUID, email TEXT, banned BOOLEAN)"
+	createUserTableIndexSql string = "CREATE INDEX IF NOT EXISTS users_table_index ON users (id, email, banned)"
 
-	createFlightTableSql      string = ""
-	createFlightTableIndexSql string = ""
+	createFlightTableSql      string = "CREATE TABLE IF NOT EXISTS flights (origin VARCHAR(10), destination VARCHAR(10), tail VARCHAR(10), flight_date DATE, user UUID)"
+	createFlightTableIndexSql string = "CREATE INDEX IF NOT EXISTS flights_table_index ON flights (origin, destination, tail, flight_date, user)"
 )
 
 type PostgresDatabase struct {
 	db *pgxpool.Pool
 }
 
-func (p PostgresDatabase) GetFlightsForUser(user model.User) []model.Flight {
+func (p *PostgresDatabase) GetFlightsForUser(user model.User) []model.Flight {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (p PostgresDatabase) AddFlight(flight model.Flight) error {
+func (p *PostgresDatabase) AddFlight(flight model.Flight) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (p PostgresDatabase) DeleteFlight(flight model.Flight) error {
+func (p *PostgresDatabase) DeleteFlight(flight model.Flight) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (p PostgresDatabase) UpdateFlight(flight model.Flight) error {
+func (p *PostgresDatabase) UpdateFlight(flight model.Flight) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (p PostgresDatabase) FillUser(user *model.User) {
+func (p *PostgresDatabase) FillUser(user *model.User) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (p PostgresDatabase) GetUser(id model.UserId) model.User {
+func (p *PostgresDatabase) GetUser(id model.UserId) model.User {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (p PostgresDatabase) DeleteUser(user model.User) error {
+func (p *PostgresDatabase) DeleteUser(user model.User) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (p PostgresDatabase) UpdateUser(user model.User) error {
+func (p *PostgresDatabase) UpdateUser(user model.User) error {
 	//TODO implement me
 	panic("implement me")
 }
@@ -74,13 +75,20 @@ func NewPostgresDatabase(connectionString string) *PostgresDatabase {
 	if poolErr != nil {
 		panic(poolErr.Error())
 	}
-	setupDatabase(pool)
+	if setupErr := setupDatabase(pool); setupErr != nil {
+		panic(setupErr.Error())
+	}
 	return &PostgresDatabase{
 		db: pool,
 	}
 }
 
-func setupDatabase(pool *pgxpool.Pool) {
-	//TODO setup database (tables index etc)
-	panic("implement me")
+func setupDatabase(pool *pgxpool.Pool) error {
+	dbSetupBatch := &pgx.Batch{}
+	dbSetupBatch.Queue(createFlightTableSql)
+	dbSetupBatch.Queue(createFlightTableIndexSql)
+	dbSetupBatch.Queue(createUserTableSql)
+	dbSetupBatch.Queue(createUserTableIndexSql)
+	results := pool.SendBatch(context.Background(), dbSetupBatch)
+	return results.Close()
 }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,7 @@
 package routes
+
+import "github.com/hschimke/planeTracker/internal/data/database"
+
+type Server struct {
+	db *database.FlightDatabase
+}
