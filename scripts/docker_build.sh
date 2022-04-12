d3a4eeb68b6f11b55b09218a3e860c52f994d2a0
Henry Schimke <henry@azule.info>
2022-04-12 20:35:59 +0000

    docker-compose build

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # MUST BE RUN FROM ROOT OF REPOSITORY
 readonly GIT_VERSION=`git rev-parse HEAD`
-docker build -t planes/web-serv -f docker/Dockerfile.api-server --build-arg "GIT_VERSION=${GIT_VERSION}" .
+docker build -t planes/api-serv -f docker/Dockerfile.api-server --build-arg "GIT_VERSION=${GIT_VERSION}" .
