fb46377b397def76ad5049382dc89e8fed6c3664
Henry Schimke <henry@azule.info>
2022-04-17 15:39:19 +0000

    passenger routes

--- web-serv/main.go
+++ web-serv/main.go
@@ -22,12 +22,23 @@ func main() {
 	routerMap := routes.NewServer(flightDatabase)
 
 	router := http.NewServeMux()
+
+	// Flight features
 	router.Handle(apiString+"getFlights", authRequiredMW(http.HandlerFunc(routerMap.GetFlightsForUser)))
 	router.Handle(apiString+"addFlight", authRequiredMW(http.HandlerFunc(routerMap.AddFlight)))
 	router.Handle(apiString+"deleteFlight", authRequiredMW(http.HandlerFunc(routerMap.DeleteFlight)))
 	router.Handle(apiString+"updateFlight", authRequiredMW(http.HandlerFunc(routerMap.UpdateFlight)))
 	router.Handle(apiString+"bulkAddFlights", authRequiredMW(http.HandlerFunc(routerMap.BulkAddFlights)))
 	router.Handle(apiString+"getPlaneDetails", authRequiredMW(http.HandlerFunc(routerMap.GetPlaneDetail)))
+
+	// Passenger Features
+	router.Handle(apiString+"getPassengersForUser", authRequiredMW(http.HandlerFunc(routerMap.GetPassengersForUser)))
+	router.Handle(apiString+"addPassengerForUser", authRequiredMW(http.HandlerFunc(routerMap.AddPassengerForUser)))
+	router.Handle(apiString+"removePassengerForUser", authRequiredMW(http.HandlerFunc(routerMap.RemovePassengerForUser)))
+	router.Handle(apiString+"setDefaultStatusForPassengerOfUser", authRequiredMW(http.HandlerFunc(routerMap.SetDefaultStatusForPassengerOfUser)))
+	router.Handle(apiString+"addPassengerToFlight", authRequiredMW(http.HandlerFunc(routerMap.AddPassengerToFlight)))
+	router.Handle(apiString+"removePassengerFromFlight", authRequiredMW(http.HandlerFunc(routerMap.RemovePassengerFromFlight)))
+	router.Handle(apiString+"getFlightsAsPassenger", authRequiredMW(http.HandlerFunc(routerMap.GetFlightsAsPassenger)))
 
 	address := fmt.Sprintf(":%s", serverPort)
 
